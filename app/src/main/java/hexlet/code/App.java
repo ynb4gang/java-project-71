9b0fc13f71a4f9e8cc7d44b2a22b6fa1067815d9
ynb4gang <129555715+ynb4gang@users.noreply.github.com>
2023-12-11 17:46:53 +0300

    Вывод справки

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App {
+    @Option(names= {"-h","--help"},usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested;
+    @Option(names = {"-V", "--Version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CommandLine cmd = new CommandLine(new App());
     }
 }
