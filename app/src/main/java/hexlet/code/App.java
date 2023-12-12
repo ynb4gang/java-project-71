16c02a8e737a629fdebb811710c210b382052cba
ynb4gang <129555715+ynb4gang@users.noreply.github.com>
2023-12-12 11:31:49 +0300

    update second step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,16 +3,38 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-class App {
+class App implements Callable<Integer> {
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
     @Option(names= {"-h","--help"},usageHelp = true, description = "Show this help message and exit.")
     private boolean helpRequested;
-    @Option(names = {"-V", "--Version"}, versionHelp = true, description = "Print version information and exit.")
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     private boolean versionRequested;
+    @Parameters(paramLabel= "filepath1", description = "path to first file")
+    private File filepath1;
+    @Parameters(paramLabel= "filepath2", description = "path to second file")
+    private File filepath2;
 
-    public static void main(String[] args) {
-        CommandLine cmd = new CommandLine(new App());
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
