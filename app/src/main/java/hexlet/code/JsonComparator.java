5add9ea482882c15f06d40bb210b8e7f32ed16f6
ynb4gang <129555715+ynb4gang@users.noreply.github.com>
2023-12-28 12:11:57 +0300

    Fifth Step

--- app/src/main/java/hexlet/code/JsonComparator.java
+++ app/src/main/java/hexlet/code/JsonComparator.java
@@ -2,35 +2,45 @@ package hexlet.code;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class JsonComparator {
-    public static Map<String, Object> comparator(Map<String, Object> parseFileOne, Map<String, Object> parseFileTwo) {
-        TreeSet<String> sortedParam = new TreeSet<>();
-        sortedParam.addAll(parseFileOne.keySet());
-        sortedParam.addAll(parseFileTwo.keySet());
 
+    public static Map<String, Object> comparator(Map<String, Object> parseFileOne, Map<String, Object> parseFileTwo) {
         Map<String, Object> diffParse = new LinkedHashMap<>();
 
-        for (String s : sortedParam) {
-            if (parseFileOne.containsKey(s) && parseFileTwo.containsKey(s)) {
-                Object firstMapValue = parseFileOne.get(s);
-                Object secondMapValue = parseFileTwo.get(s);
-                if (firstMapValue.equals(secondMapValue)) {
-                    diffParse.put("  " + s, firstMapValue);
-                } else {
-                    diffParse.put("- " + s, firstMapValue);
-                    diffParse.put("+ " + s, secondMapValue);
-                }
-            } else {
-                if (parseFileOne.containsKey(s) && !parseFileTwo.containsKey(s)) {
-                    diffParse.put("- " + s, parseFileOne.get(s));
-                }
-                if (!parseFileOne.containsKey(s) && parseFileTwo.containsKey(s)) {
-                    diffParse.put("+ " + s, parseFileTwo.get(s));
-                }
-            }
+        Set<String> allKeys = new TreeSet<>(parseFileOne.keySet());
+        allKeys.addAll(parseFileTwo.keySet());
+
+        for (String key : allKeys) {
+            compareKeys(key, parseFileOne, parseFileTwo, diffParse);
         }
+
         return diffParse;
+    }
+
+    private static void compareKeys(String key, Map<String, Object> parseFileOne, Map<String, Object> parseFileTwo, Map<String, Object> diffParse) {
+        if (parseFileOne.containsKey(key) && parseFileTwo.containsKey(key)) {
+            Object firstMapValue = parseFileOne.get(key);
+            Object secondMapValue = parseFileTwo.get(key);
+            if (firstMapValue.equals(secondMapValue)) {
+                diffParse.put("  " + key, firstMapValue);
+            } else {
+                diffParse.put("- " + key, firstMapValue);
+                diffParse.put("+ " + key, secondMapValue);
+            }
+        } else {
+            handleMissingKey(key, parseFileOne, parseFileTwo, diffParse);
+        }
+    }
+
+    private static void handleMissingKey(String key, Map<String, Object> parseFileOne, Map<String, Object> parseFileTwo, Map<String, Object> diffParse) {
+        if (parseFileOne.containsKey(key)) {
+            diffParse.put("- " + key, parseFileOne.get(key));
+        }
+        if (parseFileTwo.containsKey(key)) {
+            diffParse.put("+ " + key, parseFileTwo.get(key));
+        }
     }
 }
