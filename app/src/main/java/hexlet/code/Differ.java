3d4cf3ed72db3946d211ca2027e437daaab29097
ynb4gang <129555715+ynb4gang@users.noreply.github.com>
2024-03-23 11:55:52 +0300

    Final Step

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -22,26 +22,12 @@ public class Differ {
         Map<String, Object> parsedFirstFile = UniversalParser.parseData(fileInputStream1, getFileExtension(filePath1));
         Map<String, Object> parsedSecondFile = UniversalParser.parseData(fileInputStream2, getFileExtension(filePath2));
 
-
         return DifferenceFormatter.format(parsedFirstFile, parsedSecondFile, format);
     }
+
     public static String generate(String filePath1, String filePath2) throws IOException {
-        Path path1 = Paths.get(filePath1).toAbsolutePath();
-        Path path2 = Paths.get(filePath2).toAbsolutePath();
-
-        if (!Files.exists(path1) || !Files.exists(path2)) {
-            return "One or both files do not exist.";
-        }
-
-        FileInputStream fileInputStream1 = new FileInputStream(path1.toFile());
-        FileInputStream fileInputStream2 = new FileInputStream(path2.toFile());
-        Map<String, Object> parsedFirstFile = UniversalParser.parseData(fileInputStream1, getFileExtension(filePath1));
-        Map<String, Object> parsedSecondFile = UniversalParser.parseData(fileInputStream2, getFileExtension(filePath2));
-
-
-        return DifferenceFormatter.format(parsedFirstFile, parsedSecondFile, "stylish");
+        return generate(filePath1, filePath2, "stylish");
     }
-
     private static String getFileExtension(String filePath) {
         int lastDotIndex = filePath.lastIndexOf('.');
         if (lastDotIndex != -1) {
